6518f6fc049dd51f3c3bae702cd8f7c32dd89b29
李崇 <qazmlp2426@qq.com>
2023-01-17 15:17:24 +0800

    三方

--- src/pages/nest/index.vue
+++ src/pages/nest/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div>三方</div>
-        <iframe id="iframe" src="http://www.fengling.xyz/actionTest1/order" style=" height: 100%" width="100%"
+        <iframe id="iframe" src="http://www.fengling.xyz/miaomiao/#/movie/NowPlaying" style=" height: 100%" width="100%"
             frameborder="0"></iframe>
     </div>
 </template>
